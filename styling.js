454297dd9887478f1d7db3b17b7066d311fbc2f7
Alton <smithiialton@gmail.com>
2024-08-27 20:18:53 -0700

    completed 2 of 5 styles

---
+++ styling.js
@@ -0,0 +1,17 @@
+function changeColor() {
+    const box = document.getElementById("box");
+    box.classList.toggle("apples"); // Toggle the color class on each click
+  }
+
+
+function changeGif() {
+  const img = document.getElementById("rick")
+  img.src= "images/The True RHM Imgur needs and deserves - Imgur.gif"
+
+}
+
+
+function backToMeme() {
+  const img = document.getElementById("rick")
+  img.src = "images/memebetter.com-20240827200314.jpg"
+}
